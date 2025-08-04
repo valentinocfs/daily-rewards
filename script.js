780f161fcc274a9c577a8319b7c4830d238de96e
valentinocfs <valentinostania@gmail.com>
2025-08-04 20:02:49 +0700

    fix: update ui and fix bug undefined keywords

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let jeda = Math.floor(Math.random() * (8000 - 6000 + 1)) + 6000; // jeda random antara 6 - 9 detik
+// let jeda = Math.floor(Math.random() * (7000 - 5000 + 1)) + 5000; // jeda random antara 5 - 7 detik
+let jeda = 0; // jeda random antara 5 - 7 detik
 let keywordStatus = [];
 let i = 0;
 const storedStatus = localStorage.getItem('keywordStatus');
@@ -14,6 +15,9 @@ if (storedStatus && storedIndex) {
   generateKeywordManual()
 }
 
+document.getElementById('current-index-keyword').innerHTML = i;
+document.getElementById('total-keyword').innerHTML = keywordStatus.length;
+
 function start() {
   if (i >= keywordStatus.length) {
     alert("Semua keyword sudah dibuka. Silakan melakukan generate keyword baru!");
@@ -24,9 +28,12 @@ function start() {
   keywordStatus[i].isOpened = true;
   
   const cvid = crypto.randomUUID(); // generate random cvid
-  window.open(`https://www.bing.com/search?q=${encodeURIComponent(words[i])}&FORM=QBLH&PC=LCTS&cvid=${cvid}&pglt=161`, '_blank');
+  window.open(`https://www.bing.com/search?q=${encodeURIComponent(keywordStatus[i].keyword)}&FORM=QBLH&PC=LCTS&cvid=${cvid}&pglt=161`, '_blank');
 
   i++;
+
+  document.getElementById('current-index-keyword').innerHTML = i;
+  document.getElementById('total-keyword').innerHTML = keywordStatus.length;
 
   localStorage.setItem('keywordStatus', JSON.stringify(keywordStatus));
   localStorage.setItem('currentIndex', i);
@@ -59,10 +66,34 @@ function showTable() {
   const tableBody = document.querySelector("#table-keywords tbody");
   tableBody.innerHTML = "";
 
-  keywordStatus.forEach((item, index) => {
+  const rawIndex = i;
+  const currentIndex = rawIndex !== null ? parseInt(rawIndex, 10) : NaN;
+  if (isNaN(currentIndex)) return;
+
+  
+  let slice = (rawIndex == 1 ? 4 : 3);
+  let start = Math.max(0, currentIndex - 1);
+  let end = Math.min(keywordStatus.length, currentIndex + slice);
+  if(rawIndex == 0){
+    start = Math.max(0, currentIndex - 1);
+    end = Math.min(keywordStatus.length, currentIndex + 5);
+  }
+
+  if(rawIndex > keywordStatus.length - slice){
+    start = keywordStatus.length - slice - 1;
+  }
+  
+  if(rawIndex > 1){
+    const dotsRow = document.createElement("tr");
+    dotsRow.innerHTML = `<td colspan="3" style="text-align:center;">...</td>`;
+    tableBody.appendChild(dotsRow);
+  }
+
+  const visibleItems = keywordStatus.slice(start, end);
+  visibleItems.forEach((item, i) => {
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td style="text-align:center;">${index + 1}</td>
+      <td style="text-align:center;">${start + i + 1}</td>
       <td>${item.keyword}</td>
       <td class="${item.isOpened ? 'opened' : 'not-opened'}">
         ${item.isOpened ? 'Sudah Dibuka' : 'Belum Dibuka'}
@@ -70,9 +101,15 @@ function showTable() {
     `;
     tableBody.appendChild(row);
   });
+
+  if((rawIndex == 0 || rawIndex >= 1) && (rawIndex < keywordStatus.length - slice)){
+    const dotsRow = document.createElement("tr");
+    dotsRow.innerHTML = `<td colspan="3" style="text-align:center;">...</td>`;
+    tableBody.appendChild(dotsRow);
+  }
 }
 
-async function generateKeyword() {
+async function generateKeywordAI() {
   // block request per hari
   let storedDateGenerate = localStorage.getItem('generatekeywordai');
   if(storedDateGenerate){
@@ -87,7 +124,7 @@ async function generateKeyword() {
     }
   }
 
-  const message = `Gantikan semua array javascript yang berisi keyword dibawah ini dan jangan dipakai lagi untuk isi array yang baru. Ganti keyword dengan jumlah 60 jangan sampai lebih atau kurang dan berikan array tanpa definisi variable const, let, atau var. Berikan hasilnya saja tanpa penjelasan atau konteks dan respon dalam bentuk array saja. ["sejarah komik jepang", "manfaat jalan kaki pagi", "tutorial desain poster canva"]`;
+  const message = `Gantikan semua array javascript yang berisi keyword dibawah ini dan jangan dipakai lagi untuk isi array yang baru. Ganti keyword dengan jumlah 35 jangan sampai lebih atau kurang dan berikan array tanpa definisi variable const, let, atau var. Berikan hasilnya saja tanpa penjelasan atau konteks dan respon dalam bentuk array saja. ["sejarah komik jepang", "manfaat jalan kaki pagi", "tutorial desain poster canva"]`;
 
   const loading = document.getElementById('loading-keyword');
   loading.innerHTML = 'Tunggu beberapa saat...';
@@ -98,13 +135,15 @@ async function generateKeyword() {
   btnGenerateAi.disabled = true;
   btnGenerateManual.disabled = true;
 
+  const API_TOKEN = '';
+
   try {
     const response = await fetch(
       'https://openrouter.ai/api/v1/chat/completions',
       {
         method: 'POST',
         headers: {
-          Authorization: 'Bearer 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',
+          Authorization: 'Bearer ' + API_TOKEN,
           'HTTP-Referer': 'https://valentinocfs.github.io/daily-rewards/',
           'X-Title': 'Daily Rewards',
           'Content-Type': 'application/json',
@@ -164,10 +203,10 @@ function generateKeywordManual(){
   // clear all keywords
   words = [];
 
-  const btnGenerateAi = document.getElementById('btn-generate-ai');
+  // const btnGenerateAi = document.getElementById('btn-generate-ai');
   const btnGenerateManual = document.getElementById('btn-generate-manual');
 
-  btnGenerateAi.disabled = true;
+  // btnGenerateAi.disabled = true;
   btnGenerateManual.disabled = true;
 
   const firstWordMap = new Map();
@@ -182,7 +221,7 @@ function generateKeywordManual(){
   const allUnique = Array.from(firstWordMap.values());
   const shuffled = allUnique.sort(() => Math.random() - 0.5); // shuffle
 
-  words = shuffled.slice(0, 60);
+  words = shuffled.slice(0, 35);
 
   // clear stored keyword
   localStorage.removeItem('keywordStatus');
@@ -194,7 +233,14 @@ function generateKeywordManual(){
   localStorage.setItem('keywordStatus', JSON.stringify(keywordStatus));
   localStorage.setItem('currentIndex', i);
 
+  document.getElementById('current-index-keyword').innerHTML = i;
+  document.getElementById('total-keyword').innerHTML = keywordStatus.length;
+
   showTable();
-  btnGenerateAi.disabled = false;
+  // btnGenerateAi.disabled = false;
   btnGenerateManual.disabled = false;
+}
+
+function startSearchNews(){
+  window.open(`https://www.bing.com/news`, '_blank');
 }
